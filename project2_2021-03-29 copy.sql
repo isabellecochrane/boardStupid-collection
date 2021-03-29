d8e61215b806e350835cd324576fd7fa5c52ec07
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-03-29 11:19:23 +0100

    spelling correctoin

---
+++ project2_2021-03-29 copy.sql
@@ -0,0 +1,60 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 5446
+#
+# https://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.33)
+# Database: project2
+# Generation Time: 2021-03-29 10:18:35 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table boardStupid
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `boardStupid`;
+
+CREATE TABLE `boardStupid` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `playing_time` int(11) DEFAULT NULL,
+  `min_players` int(11) DEFAULT NULL,
+  `max_players` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `boardStupid` WRITE;
+/*!40000 ALTER TABLE `boardStupid` DISABLE KEYS */;
+
+INSERT INTO `boardStupid` (`id`, `name`, `rating`, `playing_time`, `min_players`, `max_players`)
+VALUES
+	(1,'Codenames',5,60,4,8),
+	(2,'Dobble',5,5,2,6),
+	(3,'Set',4,10,2,6),
+	(4,'Monopoly',3,120,3,8),
+	(5,'Articulate',4,30,4,8);
+
+/*!40000 ALTER TABLE `boardStupid` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
