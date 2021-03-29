511c6a2dc027a1d0a5f3cae984371b96962bc5f7
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-03-29 12:25:13 +0100

    database link

---
+++ boardStupid.php
@@ -0,0 +1,11 @@
+<?php
+
+$db = new PDO('mysql:host=db; dbname=boardStupid', 'root', 'password');
+$query = $db->prepare('QUERY;');
+$query->execute();
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare('SELECT * FROM `boardStupid`;');
+$query->execute();
+$result = $query->fetchAll();
+
