88e35cfce10fa11c83b3f4ac8c4b0e48abdcd498
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-03-30 12:09:43 +0100

    function concatantion

--- game_function.php
+++ game_function.php
@@ -15,14 +15,14 @@ if (empty($result)){
     return 'There are no games to display';
 } else {
     foreach ($result as $data) {
-        '<div class="game">';
-        $game_data .= '<h3>Game: ' . $data['name'] . '</h3>';
-        '<div class="info">';
-        $game_data .= '<div>Rating: ' . $data['rating'] . '/5' . '</div>';
-        $game_data .= '<div>game duration: ' . $data['playing_time'] . ' minutes' . '</div>';
-        $game_data .= '<div>minimum players: ' . $data['min_players'] . '</div>';
-        $game_data .= '<div>maximum players: ' . $data['max_players'] . '</div>';
-        '</div>';
+        $game_data .='<div class="game">';
+            $game_data .= '<h3>Game: ' . $data['name'] . '</h3>';
+            $game_data .='<div class="info">';
+                $game_data .= '<div>Rating: ' . $data['rating'] . '/5' . '</div>';
+                $game_data .= '<div>game duration: ' . $data['playing_time'] . ' minutes' . '</div>';
+                $game_data .= '<div>minimum players: ' . $data['min_players'] . '</div>';
+                $game_data .= '<div>maximum players: ' . $data['max_players'] . '</div>';
+            '</div>';
         '</div>';
     }
 
