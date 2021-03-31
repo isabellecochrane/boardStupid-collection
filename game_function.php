f0f5cc7985a543d2a9a28e722d94363bb0aa3c58
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-03-31 14:26:39 +0100

    indentation

--- game_function.php
+++ game_function.php
@@ -37,18 +37,17 @@ if (empty($result)) {
 foreach ($result as $data) {
     if (!$data['deleted']) {
         $game_data .= '<div class="game">';
-        $game_data .= '<h3>Game: ' . $data['name'] . '</h3>';
-        $game_data .= '<div class="info">';
-        $game_data .= '<div>Rating: ' . $data['rating'] . '/5' . '</div>';
-        $game_data .= '<div>game duration: ' . $data['playing_time'] . ' minutes' . '</div>';
-        $game_data .= '<div>minimum players: ' . $data['min_players'] . '</div>';
-        $game_data .= '<div>maximum players: ' . $data['max_players'] . '</div>';
-        $game_data .= '</div>';
-        $game_data .= '<a href="delete.php?id=' . $data['id'] . '">delete</a>';
+            $game_data .= '<h3>Game: ' . $data['name'] . '</h3>';
+            $game_data .= '<div class="info">';
+                $game_data .= '<div>Rating: ' . $data['rating'] . '/5' . '</div>';
+                $game_data .= '<div>game duration: ' . $data['playing_time'] . ' minutes' . '</div>';
+                $game_data .= '<div>minimum players: ' . $data['min_players'] . '</div>';
+                $game_data .= '<div>maximum players: ' . $data['max_players'] . '</div>';
+            $game_data .= '</div>';
+            $game_data .= '<a href="delete.php?id=' . $data['id'] . '">delete</a>';
         $game_data .= '</div>';
 
 }
-
 }
     return $game_data;
 }
